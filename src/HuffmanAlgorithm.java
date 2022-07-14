680fd5b19faee567132ef8ecbc6005792dff96b2
hoomanzolfaghari <hoomanzolfaghari84@gmail.com>
2022-07-14 18:32:18 +0430

    first code commit

---
+++ src/HuffmanAlgorithm.java
@@ -0,0 +1,21 @@
+public class HuffmanAlgorithm {
+
+    // returns the compressed format of the text consisting of bits
+    public String compressText(String text){
+
+        return null;
+    }
+
+    // returns the text extracted from compressed text which was compressed by huffman algorithm and consists bits
+    public String extractText(String compressedText){
+
+        return null;
+    }
+
+
+    class Node{
+
+    }
+
+}
+
